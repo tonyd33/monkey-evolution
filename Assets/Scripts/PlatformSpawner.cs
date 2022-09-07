9617a6923f49860e61831e3f5a939a282d162423
Tony <tonydu121@hotmail.com>
2022-09-07 14:38:11 -0400

    prototype platform functionality

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     public GameObject platformPrefab;
+    private int time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,14 @@ public class PlatformSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += 1;
+        if (time % 5000 == 0) {
+            Instantiate(
+                    platformPrefab,
+                    Vector3.zero,
+                    Quaternion.identity,
+                    gameObject.transform
+                    );
+        }
     }
 }
